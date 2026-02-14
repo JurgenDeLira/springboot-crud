feab86126ed5484a0c06886de2080acf7d4c6d92
JurgenDeLira <arq.jorgedelira@gmail.com>
2026-02-14 12:22:53 -0700

    Api Restful en Postman

--- src/main/java/com/jorge/springboot/app/springboot_crud/controllers/ProductController.java
+++ src/main/java/com/jorge/springboot/app/springboot_crud/controllers/ProductController.java
@@ -38,6 +38,7 @@ public class ProductController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
+        product.setId(id);
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(product));
     }
 
