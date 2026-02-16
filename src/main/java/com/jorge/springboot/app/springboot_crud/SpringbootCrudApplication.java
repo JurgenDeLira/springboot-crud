51f4cbc30d237783e5bfbac66da8bb8c07120235
JurgenDeLira <arq.jorgedelira@gmail.com>
2026-02-16 16:43:55 -0700

    mensajes de error personalizados usando properties

--- src/main/java/com/jorge/springboot/app/springboot_crud/SpringbootCrudApplication.java
+++ src/main/java/com/jorge/springboot/app/springboot_crud/SpringbootCrudApplication.java
@@ -2,8 +2,10 @@ package com.jorge.springboot.app.springboot_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+
 public class SpringbootCrudApplication {
 
     public static void main(String[] args) {
