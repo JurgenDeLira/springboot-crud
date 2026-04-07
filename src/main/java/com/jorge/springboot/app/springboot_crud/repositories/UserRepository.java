9f6c0da8b2c971f027b60a000ade914d5574ada5
JurgenDeLira <arq.jorgedelira@gmail.com>
2026-04-07 14:59:06 -0700

    UserDetailsService

--- src/main/java/com/jorge/springboot/app/springboot_crud/repositories/UserRepository.java
+++ src/main/java/com/jorge/springboot/app/springboot_crud/repositories/UserRepository.java
@@ -3,6 +3,9 @@ package com.jorge.springboot.app.springboot_crud.repositories;
 import com.jorge.springboot.app.springboot_crud.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
     boolean existsByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
