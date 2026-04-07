31670584fe7181bf234a2fdfe125dde2d4859cf5
JurgenDeLira <arq.jorgedelira@gmail.com>
2026-04-07 12:48:42 -0700

    ManyToMany Bidireccional

--- src/main/java/com/jorge/springboot/app/springboot_crud/entities/User.java
+++ src/main/java/com/jorge/springboot/app/springboot_crud/entities/User.java
@@ -1,6 +1,7 @@
 package com.jorge.springboot.app.springboot_crud.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
@@ -8,6 +9,7 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -28,6 +30,7 @@ public class User {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
+    @JsonIgnoreProperties({"users", "handler", "hibernateLazyInitializer"})
     @ManyToMany
     @JoinTable(
             name = "users_roles",
@@ -36,6 +39,10 @@ public class User {
             uniqueConstraints = { @UniqueConstraint(columnNames = {"user_id", "role_id"})}
     )
     private List<Role> roles;
+
+    public User() {
+        roles = new ArrayList<>();
+    }
 
 
     private boolean enabled;
@@ -49,5 +56,34 @@ public class User {
         enabled = true;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals (Object obj){
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if(getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null){
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (username == null) {
+            return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
+    }
 
 }
