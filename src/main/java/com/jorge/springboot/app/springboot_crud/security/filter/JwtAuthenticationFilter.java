dc0a437cc61623b002c7da48b67958c44170ef83
JurgenDeLira <arq.jorgedelira@gmail.com>
2026-04-15 15:22:09 -0700

    firmando el token jwt

--- src/main/java/com/jorge/springboot/app/springboot_crud/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/jorge/springboot/app/springboot_crud/security/filter/JwtAuthenticationFilter.java
@@ -1,6 +1,11 @@
 package com.jorge.springboot.app.springboot_crud.security.filter;
 
 import com.jorge.springboot.app.springboot_crud.entities.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,11 +17,17 @@ import tools.jackson.core.exc.StreamReadException;
 import tools.jackson.databind.DatabindException;
 import tools.jackson.databind.ObjectMapper;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private AuthenticationManager authenticationManager;
+
+    private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
 
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
@@ -45,5 +56,29 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
                 password);
         return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain chain, Authentication authResult)
+            throws IOException, ServletException {
+
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) authResult.getPrincipal();
+        String username = user.getUsername();
+        String token = Jwts.builder()
+                .subject(username)
+                .signWith(SECRET_KEY)
+                .compact();
+
+        response.addHeader("Authorization", "Bearer " + token);
+
+        Map<String, String> body = new HashMap<>();
+        body.put("token", token);
+        body.put("username", username);
+        body.put("message", String.format("Hola %s has iniciado sesión con éxito!", username));
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType("application/json");
+        response.setStatus(200);
     }
 }
